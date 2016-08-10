420ddf502fc23392ca0e21e3d27efd3e9d28f97e
Thomas Rueckstiess <thomas@rueckstiess.net>
2016-08-10 20:23:44 +0200

    rename to index.jsx

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require('./lib/d3component');
+module.exports = require('./lib');
---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('D3Component', function() {
+  it('dummy test, always passes', function() {
+    assert(true);
+  });
+});
